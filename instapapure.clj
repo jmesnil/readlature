9ed51bc130ab3854daad928f5ab09a1858036da4
Jeff Mesnil <jmesnil@gmail.com>
2009-08-11 22:26:52 +0200

    added POST method on /post

--- instapapure.clj
+++ instapapure.clj
@@ -15,11 +15,18 @@
     (html 
       [:strong "Pong"])))
   
+(defn post [location title]
+  (layout (str "Posted " title) 
+    (html 
+      [:a {:href location} title])))  
+
 (defroutes instapapure-app
    (GET "/public/*"
      (or (serve-file (params :*)) :next))
    (GET "/ping" 
      (ping))
+   (POST "/post" 
+       (post (:l params) (:t params)))
    (ANY "*" 
      (page-not-found)))
 
