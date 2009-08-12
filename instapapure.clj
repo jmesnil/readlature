5ef5c338c6ebc6f9ee0405dec8b035e2ff26e1cf
Jeff Mesnil <jmesnil@gmail.com>
2009-08-12 22:27:51 +0200

    "Read later" bookmarklet
    * added draggable bookmarklet to the posts page
    * added bookmarklet.js which contains the readable javascript code.
      the bookmarklet in instapapure.clj is generated from the readable
      one by TextMate

--- instapapure.clj
+++ instapapure.clj
@@ -39,7 +39,10 @@
 ;; ********* ;;
 ;; Web Layer ;;
 ;; ********* ;;
-  
+
+(def bookmarklet 
+  "javascript:var%20d=document,uri%20=%20%27http://localhost:8080/post%27;f%20=%20d.createElement(%27form%27);f.method%20=%20%27post%27;f.action%20=%20uri;t%20=d.createElement(%27input%27);t.type%20=%20%27hidden%27;t.name%20=%20%27t%27;t.value%20=d.title;l%20=%20d.createElement(%27input%27);l.type%20=%20%27hidden%27;l.name%20=%20%27l%27;l.value%20=%20d.location.href;f.appendChild(t);f.appendChild(l);b%20=%20d.createElement(%27body%27);b.appendChild(f);h%20=d.getElementsByTagName(%27html%27)[0];h.appendChild(b);f.submit();void(0)")
+
 (defn layout [title & body]
   (html
     [:head
@@ -61,7 +64,9 @@
     (html
       [:ul
         (map display-post (select-posts))]
-      [:a {:href "/post"} "New Post"])))      
+      [:a {:href "/post"} "New Post"]
+      [:p "Drag the bookmarklet: "
+        [:a {:href bookmarklet} "Read later"]])))      
 
 (defn new-post [location title]
   (insert-post location title)
