178713619829f12d613969019d718d297787b2a9
Jeff Mesnil <jmesnil@gmail.com>
2009-08-13 12:12:52 +0200

    added a "summary"
    * added a summary field in the posts DB table
    * added a 's' parameter to the POST method on /post
    * in the bookmarklet, the summary 's' is set by calling getSelection()
      if the user selects a portion of text, it will be used as the
      post's summary (tested on Safari & Firefox)

--- instapapure.clj
+++ instapapure.clj
@@ -14,9 +14,10 @@
 
 (defn create-instapapure-tables []
   (create-table :posts
-    [:id :int "IDENTITY" "PRIMARY KEY"]
-    [:location :varchar "NOT NULL"]
-    [:title :varchar "NOT NULL"]
+    [:id         :int "IDENTITY" "PRIMARY KEY"]
+    [:location   :varchar "NOT NULL"]
+    [:title      :varchar "NOT NULL"]
+    [:summary    :varchar "NOT NULL"]
     [:created_at :datetime]))
 
 ;; we put the table creation in a try.
@@ -25,12 +26,12 @@
   (with-connection db (create-instapapure-tables))
   (catch Exception _))
 
-(defn insert-post [location title]
+(defn insert-post [location title summary]
   (with-connection db
     (transaction
       (insert-values :posts
-        [:location :title :created_at]
-        [ location   title  (now)]))))
+        [:location :title :summary :created_at]
+        [location  title  summary  (now)]))))
 
 (defn select-posts []
   (with-connection db
@@ -41,7 +42,8 @@
 ;; ********* ;;
 
 (def bookmarklet 
-  "javascript:var%20d=document,uri%20=%20%27http://localhost:8080/post%27;f%20=%20d.createElement(%27form%27);f.method%20=%20%27post%27;f.action%20=%20uri;t%20=d.createElement(%27input%27);t.type%20=%20%27hidden%27;t.name%20=%20%27t%27;t.value%20=d.title;l%20=%20d.createElement(%27input%27);l.type%20=%20%27hidden%27;l.name%20=%20%27l%27;l.value%20=%20d.location.href;f.appendChild(t);f.appendChild(l);b%20=%20d.createElement(%27body%27);b.appendChild(f);h%20=d.getElementsByTagName(%27html%27)[0];h.appendChild(b);f.submit();void(0)")
+  "javascript:var%20d=document,uri%20=%20%27http://localhost:8080/post%27;f%20=%20d.createElement(%27form%27);f.method%20=%20%27post%27;f.action%20=%20uri;t%20=d.createElement(%27input%27);t.type%20=%20%27hidden%27;t.name%20=%20%27t%27;t.value%20=d.title;l%20=%20d.createElement(%27input%27);l.type%20=%20%27hidden%27;l.name%20=%20%27l%27;l.value%20=%20d.location.href;s%20=%20d.createElement(%27input%27);s.type%20=%20%27hidden%27;s.name%20=%20%27s%27;s.value%20=%20getSelection%20();f.appendChild(t);f.appendChild(l);f.appendChild(s);b%20=%20d.createElement(%27body%27);b.appendChild(f);h%20=d.getElementsByTagName(%27html%27)[0];h.appendChild(b);f.submit();void(0)"
+)
 
 (defn layout [title & body]
   (html
@@ -57,6 +59,7 @@
   [:li
     [:span "(" (:id post) ") "]
     [:a {:href (:location post)} (:title post)]
+    [:div (:summary post)]
     [:div " created at " (:created_at post)]])
 
 (defn show-posts []
@@ -68,8 +71,8 @@
       [:p "Drag the bookmarklet: "
         [:a {:href bookmarklet} "Read later"]])))
 
-(defn new-post [location title]
-  (insert-post location title)
+(defn new-post [location title summary]
+  (insert-post location title summary)
   (redirect-to location))
 
 (defn create-post []
@@ -81,6 +84,9 @@
       [:br]
       [:label "Title:"]
       [:input {:type "text" :name "t"}]
+      [:br]
+      [:label "Summary:"]
+      (text-area "s")
       [:br]
       (submit-button "save"))))
 
@@ -94,7 +100,7 @@
   (GET "/post"
     (create-post))
   (POST "/post"
-    (new-post (:l params) (:t params)))
+    (new-post (:l params) (:t params) (:s params)))
   (ANY "*"
     (page-not-found)))
 
