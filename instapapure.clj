823b70c2a445c9f466bfc6e1a967af7dc3e64bc8
Jeff Mesnil <jmesnil@gmail.com>
2009-08-13 21:36:19 +0200

    Star persistence
    * persist the post starred status in the db
    * added POST method on /post/star to update the starred
      of a post
    * bind the start update to the star anchor with jQuery
    * added page listing only the starred posts at "/s"
    
    redesigned UI layout by adding a header common to all pages

--- instapapure.clj
+++ instapapure.clj
@@ -18,6 +18,7 @@
     [:location   :varchar "NOT NULL"]
     [:title      :varchar "NOT NULL"]
     [:summary    :varchar "NOT NULL"]
+    [:starred    :boolean]
     [:created_at :datetime]))
 
 ;; we put the table creation in a try.
@@ -30,16 +31,24 @@
   (with-connection db
     (transaction
       (insert-values :posts
-        [:location :title :summary :created_at]
-        [location  title  summary  (now)]))))
+        [:location :title :summary :starred :created_at]
+        [location  title  summary  nil      (now)]))))
 
 (defn remove-post [id]
   (with-connection db
     (delete-rows :posts ["id=?" id])))
 
-(defn select-posts []
+(defn update-star [id starred]
   (with-connection db
-    (with-query-results res ["select * from posts"] (doall res))))
+      (update-values :posts ["id=?" id] {:starred starred})))
+
+(defn select-posts
+  ([]
+    (with-connection db
+      (with-query-results res ["select * from posts"] (doall res))))
+  ([criteria]
+    (with-connection db
+      (with-query-results res [(str "select * from posts where " criteria)] (doall res)))))
 
 ;; ********* ;;
 ;; Web Layer ;;
@@ -49,6 +58,19 @@
   "javascript:var%20d=document,uri%20=%20%27http://localhost:8080/post%27;f%20=%20d.createElement(%27form%27);f.method%20=%20%27post%27;f.action%20=%20uri;t%20=d.createElement(%27input%27);t.type%20=%20%27hidden%27;t.name%20=%20%27t%27;t.value%20=d.title;l%20=%20d.createElement(%27input%27);l.type%20=%20%27hidden%27;l.name%20=%20%27l%27;l.value%20=%20d.location.href;s%20=%20d.createElement(%27input%27);s.type%20=%20%27hidden%27;s.name%20=%20%27s%27;s.value%20=%20getSelection%20();f.appendChild(t);f.appendChild(l);f.appendChild(s);b%20=%20d.createElement(%27body%27);b.appendChild(f);h%20=d.getElementsByTagName(%27html%27)[0];h.appendChild(b);f.submit();void(0)"
 )
 
+(defn header [title]
+  (html
+    [:h1 title]
+    [:p
+      [:a {:href "/"}  "All Posts"]
+      "&nbsp;"
+      [:a {:href "/s"} "Starred Posts"]]
+    [:p
+      [:a {:href "/post"} "New Post"]
+      [:br]
+      "Drag the bookmarklet: "
+      [:a {:href bookmarklet} "Read later"]]))
+
 (defn layout [title & body]
   (html
     [:head
@@ -57,17 +79,29 @@
                   "public/j/instapapure.js")
       (include-css "public/s/instapapure.css")]
     [:body
-      [:h1 title]
+      (header title)
       body]))
 
 (defn delete-post [id]
   (remove-post id))
 
+(defn star-post [id starred]
+  (update-star id starred))
+
 (defn display-post
-  [{id :id location :location title :title summary :summary created_at :created_at}]
+  [{id         :id
+    location   :location
+    title      :title
+    summary    :summary
+    starred    :starred
+    created_at :created_at}]
 
   [:div.post
     [:div
+      (if starred
+          [:a {:class "star starred"   :title "Star it"  } "&#9733;"]
+          [:a {:class "star unstarred" :title "Unstar it"} "&#9734;"])
+      "&nbsp;"
       [:a.title {:href location :id id} title]
       "&nbsp;"
       [:a.delete { :title "Permanently Delete"} "&#10006;"]
@@ -76,21 +110,18 @@
 
 (defn show-posts []
   (layout "All Posts"
-    (html
-      [:p
-        [:a {:href "/post"} "New Post"]]
-      [:p
-        "Drag the bookmarklet: "
-        [:a {:href bookmarklet} "Read later"]]
-      (map display-post (select-posts)))))
+    (map display-post (select-posts))))
+
+(defn show-starred-posts []
+  (layout "Starred Posts"
+    (map display-post (select-posts "starred = true"))))
 
 (defn new-post [location title summary]
   (insert-post location title summary)
   (redirect-to location))
 
 (defn create-post []
-  (layout
-    "Save a page"
+  (layout "New Post"
     (form-to [:post "/post"]
       [:label "Location:"]
       [:input {:type "text" :name "l"}]
@@ -108,6 +139,8 @@
     (or (serve-file (params :*)) :next))
   (GET "/"
     (show-posts))
+  (GET "/s"
+    (show-starred-posts))
   (GET "/posts"
     (show-posts))
   (GET "/post"
@@ -116,6 +149,8 @@
     (new-post (:l params) (:t params) (:s params)))
   (POST "/post/delete"
     (delete-post (:id params)))
+  (POST "/post/star"
+    (star-post (:id params) (:s params)))
   (ANY "*"
     (page-not-found)))
 
--- public/j/instapapure.js
+++ public/j/instapapure.js
@@ -1,5 +1,26 @@
 jQuery(document).ready(function() {
 
+$("a.star").click(function() {
+  star = $(this);
+  id = star.next("a").attr("id");
+  starred = star.hasClass("starred")
+  if (starred) {
+    star.html("&#9734;");
+  } else {
+    star.html("&#9733;");
+  }
+  toggleStar(id, !starred)
+  star.toggleClass("starred");
+});
+
+toggleStar = function(id, starred) {
+  $.post("/post/star", {id: id, s: starred});
+}
+
+$("a.star").mouseover(function() {
+  $(this).addClass("cursored");
+});
+
 $("a.delete").mouseover(function() {
   $(this).addClass("cursored");
 });
