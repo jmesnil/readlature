f9a119d0aa1424c2ff316eccfbc01e76c34e28e8
Jeff Mesnil <jmesnil@gmail.com>
2009-08-21 00:09:27 +0200

    fixed helper sections
    * display "brute" images without CSS classes to prevent jQuery to act on "click"
    * added red cross image to archive helper section

--- src/instapapure/servlet.clj
+++ src/instapapure/servlet.clj
@@ -51,6 +51,9 @@
 
 (def nav-separator "&nbsp;&#9826;&nbsp;")
 
+(def delete-image
+  [:img.delete {:src "/public/i/cross.png" :title "Delete it"}])
+
 (defn star-image [displayed]
   [:img.star {:src "/public/i/star.png"
               :title "Unstar it"
@@ -59,8 +62,6 @@
   [:img.star-empty {:src "/public/i/star-empty.png"
               :title "Star it"
               :style (str "display: " (if displayed "inline" "none"))}])
-(def delete-image
-  [:img.delete {:src "/public/i/cross.png" :title "Delete it"}])
 
 (defn nav-header [type header-type header-title href]
   (if (= type header-type)
@@ -161,17 +162,19 @@
     [:div.help
       [:p [:em "Nothing in the archive?"]]
       [:p "When you have read an unread article, it will be moved to the archive.
-          It will remain there until you delete it."]]))
+          It will remain there until you delete it by clicking on " 
+          [:img {:src "/public/i/cross.png"}]
+          " on the right of the article's title."]]))
 
 (defn starred-help []
   (html
     [:div.help
       [:p [:em "Nothing starred?"]]
       [:p "If you find a good article you really like, you can \"star\" it by
-        clicking on " (star-empty-image true) " at the left of the article. Starred article will always
-        be displayed in the " (link-to "/starred" "Starred") " section
-        whether they have been read or not"]
-      [:p "To \"unstar\" a starred article, click on " (star-image true) "."]]))
+        clicking on " [:img {:src "/public/i/star-empty.png"}] " at the left of the article's title.
+        Starred article will always be displayed in the " 
+        (link-to "/starred" "Starred") " section whether they have been read or not"]
+      [:p "To \"unstar\" a starred article, click on " [:img {:src "/public/i/star.png"}]  "."]]))
 
 (defn articles-page
   [type articles advice-section]
