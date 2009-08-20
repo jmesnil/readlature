38502871b72d27671adccb03fe883e546cb2dd40
Jeff Mesnil <jmesnil@gmail.com>
2009-08-20 16:36:51 +0200

    added Google Analytics

--- src/instapapure/servlet.clj
+++ src/instapapure/servlet.clj
@@ -23,6 +23,18 @@
   "javascript:var%20d=document,uri%20=%20%27http://instapapure.appspot.com/api/article/new%27;f%20=%20d.createElement(%27form%27);f.method%20=%20%27get%27;f.action%20=%20uri;t%20=d.createElement(%27input%27);t.type%20=%20%27hidden%27;t.name%20=%20%27title%27;t.value%20=%20d.title;l%20=%20d.createElement(%27input%27);l.type%20=%20%27hidden%27;l.name%20=%20%27location%27;l.value%20=%20d.location.href;s%20=%20d.createElement(%27input%27);s.type%20=%20%27hidden%27;s.name%20=%20%27summary%27;s.value%20=%20getSelection%20();f.appendChild(t);f.appendChild(l);f.appendChild(s);b%20=%20d.createElement(%27body%27);b.appendChild(f);h%20=d.getElementsByTagName(%27html%27)[0];h.appendChild(b);f.submit();void(0)"
 )
 
+(def google-analytics
+  "<script type=\"text/javascript\">
+  var gaJsHost = ((\"https:\" == document.location.protocol) ? \"https://ssl.\" : \"http://www.\");
+  document.write(unescape(\"%3Cscript src='\" + gaJsHost + \"google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E\"));
+  </script>
+  <script type=\"text/javascript\">
+  try {
+  var pageTracker = _gat._getTracker(\"UA-257783-8\");
+  pageTracker._trackPageview();
+  } catch(err) {}</script>"
+)
+
 (defn header
   "HTML header common to all pages"
   [title]
@@ -67,7 +79,8 @@
     [:body
         (header title)
         body
-        (footer)]))
+        (footer)
+        google-analytics]))
 
 (defn delete-article
   "Delete the article specified by the id parameter"
