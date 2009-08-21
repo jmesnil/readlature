4c1c6d1e4cc1d9fb67a12f21fd2c7814742917a1
Jeff Mesnil <jmesnil@gmail.com>
2009-08-21 12:55:06 +0200

    Renamed project to 'readlature'
    * renamed files
    * updated UI
    
    + updated version to 9

--- src/readlature/articles.clj
+++ src/readlature/articles.clj
@@ -12,7 +12,7 @@
 ;; See the License for the specific language governing permissions and
 ;; limitations under the License.
 
-(ns instapapure.articles
+(ns readlature.articles
   (:require [appengine.datastore :as ds])
   (:import (com.google.appengine.api.datastore KeyFactory Query Query$FilterOperator)))
 
--- src/readlature/servlet.clj
+++ src/readlature/servlet.clj
@@ -12,13 +12,13 @@
 ;; See the License for the specific language governing permissions and
 ;; limitations under the License.
 
-(ns instapapure.servlet
+(ns readlature.servlet
   (:gen-class
     :extends javax.servlet.http.HttpServlet)
   (:use clojure.set
         compojure)
   (:require
-    [instapapure.articles :as articles]
+    [readlature.articles :as articles]
     [appengine.users :as users]))
 
 ;; ************* ;;
@@ -26,7 +26,7 @@
 ;; ************* ;;
 
 (def author {:name "Jeff Mesnil" :address "http://jmesnil.net/"})
-(def app-name "Instapapure")
+(def app-name "Read Lature")
 
 ;; ********* ;;
 ;; Web Layer ;;
@@ -34,7 +34,7 @@
 
 (def bookmarklet
 ;;   "javascript:var%20d=document,uri%20=%20%27http://localhost:8080/api/article/new%27;f%20=%20d.createElement(%27form%27);f.method%20=%20%27get%27;f.action%20=%20uri;t%20=d.createElement(%27input%27);t.type%20=%20%27hidden%27;t.name%20=%20%27title%27;t.value%20=%20d.title;l%20=%20d.createElement(%27input%27);l.type%20=%20%27hidden%27;l.name%20=%20%27location%27;l.value%20=%20d.location.href;s%20=%20d.createElement(%27input%27);s.type%20=%20%27hidden%27;s.name%20=%20%27summary%27;s.value%20=%20getSelection%20();f.appendChild(t);f.appendChild(l);f.appendChild(s);b%20=%20d.createElement(%27body%27);b.appendChild(f);h%20=d.getElementsByTagName(%27html%27)[0];h.appendChild(b);f.submit();void(0)"
-  "javascript:var%20d=document,uri%20=%20%27http://instapapure.appspot.com/api/article/new%27;f%20=%20d.createElement(%27form%27);f.method%20=%20%27get%27;f.action%20=%20uri;t%20=d.createElement(%27input%27);t.type%20=%20%27hidden%27;t.name%20=%20%27title%27;t.value%20=%20d.title;l%20=%20d.createElement(%27input%27);l.type%20=%20%27hidden%27;l.name%20=%20%27location%27;l.value%20=%20d.location.href;s%20=%20d.createElement(%27input%27);s.type%20=%20%27hidden%27;s.name%20=%20%27summary%27;s.value%20=%20getSelection%20();f.appendChild(t);f.appendChild(l);f.appendChild(s);b%20=%20d.createElement(%27body%27);b.appendChild(f);h%20=d.getElementsByTagName(%27html%27)[0];h.appendChild(b);f.submit();void(0)"
+  "javascript:var%20d=document,uri%20=%20%27http://readlature.appspot.com/api/article/new%27;f%20=%20d.createElement(%27form%27);f.method%20=%20%27get%27;f.action%20=%20uri;t%20=d.createElement(%27input%27);t.type%20=%20%27hidden%27;t.name%20=%20%27title%27;t.value%20=%20d.title;l%20=%20d.createElement(%27input%27);l.type%20=%20%27hidden%27;l.name%20=%20%27location%27;l.value%20=%20d.location.href;s%20=%20d.createElement(%27input%27);s.type%20=%20%27hidden%27;s.name%20=%20%27summary%27;s.value%20=%20getSelection%20();f.appendChild(t);f.appendChild(l);f.appendChild(s);b%20=%20d.createElement(%27body%27);b.appendChild(f);h%20=d.getElementsByTagName(%27html%27)[0];h.appendChild(b);f.submit();void(0)"
 )
 
 (def google-analytics
@@ -89,7 +89,7 @@
         (link-to "/article" "New Article")
         nav-separator
         "Drag the bookmarklet: "
-        (link-to bookmarklet "Read later")]
+        (link-to bookmarklet "Read Lature")]
       [:div.signout
         "logged as "
         [:span.user (users/current-user)]
@@ -106,8 +106,8 @@
     [:head
       [:title app-name]
       (include-js "/public/j/jquery.js"
-                  "/public/j/instapapure.js")
-      (include-css "/public/s/instapapure.css")
+                  "/public/j/readlature.js")
+      (include-css "/public/s/readlature.css")
       "<meta name=viewport content='initial-scale=1.0'>"]
     [:body
         (header title)
@@ -151,7 +151,7 @@
   (html
     [:div.help
       [:p [:em "Nothing to read?"]]
-      [:p "Drag the bookmarklet " (link-to bookmarklet "Read later") "
+      [:p "Drag the bookmarklet " (link-to bookmarklet "Read Lature") "
         in your menu bar and use it next time you find a good article you want
         to read later"]
       [:p "You can also save articles directly from Google Reader (see the "
@@ -243,7 +243,7 @@
         [:br]
         (submit-button "save")))))
 
-(defroutes instapapure-app
+(defroutes readlature-app
   ;; Web Site
   (GET "/public/*"
     (or (serve-file (params :*)) :next))
@@ -268,5 +268,5 @@
   (ANY "*"
     (page-not-found)))
 
-;; (defservice instapapure-app)
-(defservice (users/wrap-requiring-login instapapure-app))
+;; (defservice readlature-app)
+(defservice (users/wrap-requiring-login readlature-app))
